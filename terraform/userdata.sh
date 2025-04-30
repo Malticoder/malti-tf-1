a8bd65350baa485146626b40216cbbe88e654f16
malti-repo-learning <matichachra684@gmail.com>
2025-04-30 22:59:10 +0530

    Fix userdata

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -3,4 +3,5 @@
 yum -y install httpd
 systemctl start httpd
 systemctl enable httpd
-echo "This is project for Vaish College for branch name ${BRANCH_NAME}" > /var/www/html/index.html
+#echo "This is project for Vaish College for branch name ${BRANCH_NAME}" > /var/www/html/index.html
+mv /home/ec2-user/project-code/* /var/www/html/
